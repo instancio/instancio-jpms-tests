1bdbfe6b5f2b9cf40523d07d219c4d0343e9a032
Arman Sharif <armandino@gmail.com>
2023-03-01 17:22:46 -0800

    Added record test

---
+++ standalone-tests/src/test/java/org/instancio/tests/jpms/standalone/PersonRecord.java
@@ -0,0 +1,4 @@
+package org.instancio.tests.jpms.standalone;
+
+public record PersonRecord(String name, int age) {
+}
--- standalone-tests/src/test/java/org/instancio/tests/jpms/standalone/StandaloneTest.java
+++ standalone-tests/src/test/java/org/instancio/tests/jpms/standalone/StandaloneTest.java
@@ -10,9 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(InstancioExtension.class)
 class StandaloneTest {
 
+    /**
+     * Instantiate bypassing constructor to verify
+     * {@code Unsafe.allocateInstance()} works as expected.
+     */
     @Test
-    void create() {
+    void createClassWithoutAvailableConstructor() {
         final ClassWithoutAvailableConstructor result = Instancio.create(ClassWithoutAvailableConstructor.class);
+
+        assertThat(result).hasNoNullFieldsOrProperties();
+    }
+
+    @Test
+    void createPersonRecord() {
+        final PersonRecord result = Instancio.create(PersonRecord.class);
 
         assertThat(result).hasNoNullFieldsOrProperties();
     }
